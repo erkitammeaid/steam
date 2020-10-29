b9cbad1bcb29961ca5226ae5061d6c43baeb0e04
erkitammeaid <erkipoiss01@gmail.com>
2020-10-29 14:05:51 +0200

    ssss

---
+++ src/components/links.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="https://store.cloudflare.steamstatic.com/public/images//gift/steamcards_promo_02.png" alt="" srcset="">
+    <ul class="uppercase text-sm text-gray-500">Kinkekaardid</ul>
+    <ul class="uppercase text-sm text-gray-500 mt-6">Soovitused
+        <li class="normal-case flex items-center text-blue-500"><span class="bg-icons h-4 w-4 inline-block mr-2"></span>Sõprade järgi</li>
+        <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-curators h-4 w-4 inline-block mr-2"></span>Kuraatorid</li>
+        <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-tags h-4 w-4 inline-block mr-2"></span>Tagid</li>
+    </ul>
+    <ul>
+        
+    </ul>
+
+</template>
